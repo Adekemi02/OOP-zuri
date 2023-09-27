c0d821f3b26711430c98e459b902bcd02a7a6759
Adisa Barakat <adisabarakatadekemi@gmail.com>
2023-09-28 00:53:14 +0100

    modified

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from token_auth import create_app
+import os
 
 
 app = create_app()
 
+# config.py or your app's configuration file
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_URI')
+
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
